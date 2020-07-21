8148ceec027497737fb389abb835f3bb6f7aedd3
Andrew Jackson <ajackson@calrom.com>
2020-07-21 10:45:52 +0100

    initial

---
+++ Library/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Class1
+    {
+        public async Task<int> TaskMethod()
+        {
+            return await this.GetValueTask();
+        }
+
+        private Task<int> GetValueTask()
+        {
+            return Task.FromResult(5);
+        }
+
+        public async ValueTask<int> ValueTaskMethod()
+        {
+            return await this.GetValueValueTask();
+        }
+
+        private ValueTask<int> GetValueValueTask()
+        {
+            return new ValueTask<int>(5);
+        }
+    }
+}
---
+++ Tests/Coverage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Coverage
+    {
+        [Test]
+        public async Task TaskTest()
+        {
+            var obj = new Library.Class1();
+            var result = await obj.TaskMethod();
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public async Task ValueTaskTest()
+        {
+            var obj = new Library.Class1();
+            var result = await obj.ValueTaskMethod();
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
